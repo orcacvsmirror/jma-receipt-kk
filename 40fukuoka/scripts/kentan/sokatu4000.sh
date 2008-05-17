1c06abc96e39cf8416ae5944a728947c4447e30f
uchi <uchi@cvs.orca.med.or.jp>
2008-05-17 21:08:31 +0900

    返戻対応

--- 40fukuoka/scripts/kentan/sokatu4000.sh
+++ 40fukuoka/scripts/kentan/sokatu4000.sh
@@ -14,6 +14,10 @@ RENNUM=0
 #        ${13} シェルＩＤ
 #        ${15} 医療機関ＩＤ 
 #        ${16} エラーファイル名 
+#        ${17} 処理年月 
+#        ${18} 対象保険者番号
+#        ${19} 処理対象区分
+#              0:全体  1:当月・月遅れ分  2:返戻分
 #-------------------------------------------#
 #
 ##      エラーファイル削除
@@ -25,11 +29,21 @@ RENNUM=0
 
         cd  ${ORCA_DIR}
 
+##      返戻分
+        if  [ ${19} -eq '0' ] || [ ${19} -eq '2' ]; then
             RENNUM=$(expr ${RENNUM} + 1) 
-            $DBSTUB -dir $LDDEFDIR/directory -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},${18},${16} > ${LOG_FILE}.log
+            $DBSTUB -dir $LDDEFDIR/directory -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},${18},2,${16} > ${LOG_FILE}.log
             if  [ -e ${16} ]; then
                 exit
             fi
+        fi
+##      当月・月遅れ分
+        if  [ ${19} -eq '0' ] || [ ${19} -eq '1' ]; then
+            $DBSTUB -dir $LDDEFDIR/directory -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},${18},1,${16} > ${LOG_FILE}.log
+            if  [ -e ${16} ]; then
+                exit
+            fi
+        fi
 
 	$DBSTUB  -dir $LDDEFDIR/directory -bd orcabt ORCBJOB -parameter JBE${12}${13},${15}
 
