532c8249d1f9052f9746450f1ba3a4a6013f2e92
uchi <uchi@cvs.orca.med.or.jp>
2008-05-25 17:34:50 +0900

    ４月改正対応

--- 17ishikawa/scripts/kentan/sokatu1700.sh
+++ 17ishikawa/scripts/kentan/sokatu1700.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-
-. /etc/jma-receipt/jma-receipt.env
+. @jma-receipt-env@
 
 PREFNAME=ishikawa
 PROGRAMID=SOKATU1700
 LOG_FILE="/var/log/jma-receipt/${15}sokatu1700"
 RENNUM=0
-#-------------------------------------------#
-#    国保総括表作成（石川）
+-------------------------------------------#
+#    国保総括表作成（茨城）
 #        $1-${11}
 #              印刷ＤＢ用定固定引数(CPORCSRTLNK.INC)
 #        ${12} ジョブＩＤ
 #        ${13} シェルＩＤ
 #        ${15} 医療機関ＩＤ 
 #        ${16} エラーファイル名 
-#        ${17} 処理年月 
-#        ${18} 対象保険者番号
 #        ${19} 処理対象区分
 #              0:全体  1:当月・月遅れ分  2:返戻分
 #-------------------------------------------#
@@ -25,28 +22,23 @@ RENNUM=0
 	echo "echo " ${16}
         if  [ -e ${16} ]; then
             rm  ${16}
-        fi
-
-        cd  ${ORCA_DIR}
-
+        fi        
 ##      返戻分
         if  [ ${19} -eq '0' ] || [ ${19} -eq '2' ]; then
-            RENNUM=$(expr ${RENNUM} + 1) 
-       	    ${DBSTUB} -record ${RECORDDIR} -dir ${LDDEFDIR}/directory -bddir ${LDDEFDIR} -db orca -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${15},${12},${13},2,${16} > ${LOG_FILE}-2.log
+            RENNUM=$(expr $RENNUM + 1) 
+            $DBSTUB -dir $LDDEFDIR/directory -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${15},${12},${13},2,${16} > ${LOG_FILE}-2.log
             if  [ -e ${16} ]; then
                 exit
             fi
         fi
-
 ##      当月・月遅れ分
         if  [ ${19} -eq '0' ] || [ ${19} -eq '1' ]; then
-            RENNUM=$(expr ${RENNUM} + 1) 
-       	    ${DBSTUB} -record ${RECORDDIR} -dir ${LDDEFDIR}/directory -bddir ${LDDEFDIR} -db orca -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${15},${12},${13},1,${16} > ${LOG_FILE}-1.log
+            RENNUM=$(expr $RENNUM + 1) 
+            $DBSTUB -dir $LDDEFDIR/directory -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${15},${12},${13},1,${16} > ${LOG_FILE}-1.log
             if  [ -e ${16} ]; then
                 exit
             fi
         fi
+        
+	$DBSTUB  -dir $LDDEFDIR/directory -bd orcabt ORCBJOB -parameter JBE${12}${13},${15}
 
-	    ${DBSTUB} -record ${RECORDDIR} -dir ${LDDEFDIR}/directory -bddir ${LDDEFDIR} -db orca -bd orcabt ORCBJOB -parameter JBE${12}${13},${15}
-
-        exit
