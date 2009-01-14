d1183458fc57356acc3d88863fd5540d08d24783
yoshi <yoshi@cvs.orca.med.or.jp>
2009-01-14 13:54:41 +0900

    特別療養費国保対応

--- 41saga/scripts/kentan/sokatu4100.sh
+++ 41saga/scripts/kentan/sokatu4100.sh
@@ -28,7 +28,7 @@ RENNUM=0
 ##      返戻分
         if  [ ${19} -eq '0' ] || [ ${19} -eq '2' ]; then
             RENNUM=$(expr ${RENNUM} + 1) 
-       	    $DBSTUB -dir $LDDEFDIR/directory -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},2,${16} > ${LOG_FILE}-2.log
+       	    $DBSTUB -dir $LDDEFDIR/directory -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},2,1,${16} > ${LOG_FILE}-2.log
             if  [ -e ${16} ]; then
                 exit
             fi
@@ -36,11 +36,29 @@ RENNUM=0
 ##      当月・月遅れ分
         if  [ ${19} -eq '0' ] || [ ${19} -eq '1' ]; then
             RENNUM=$(expr ${RENNUM} + 1) 
-       	    $DBSTUB -dir $LDDEFDIR/directory -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},1,${16} > ${LOG_FILE}-1.log
+       	    $DBSTUB -dir $LDDEFDIR/directory -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},1,1,${16} > ${LOG_FILE}-1.log
             if  [ -e ${16} ]; then
                 exit
             fi
         fi
+
+##      返戻分（特別療養費分）
+        if  [ ${19} -eq '0' ] || [ ${19} -eq '2' ]; then
+            RENNUM=$(expr $RENNUM + 1) 
+            $DBSTUB -dir $LDDEFDIR/directory -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},2,2,${16} > ${LOG_FILE}-22.log
+            if  [ -e ${16} ]; then
+                exit
+            fi
+        fi
+##      当月・月遅れ分（特別療養費分）
+        if  [ ${19} -eq '0' ] || [ ${19} -eq '1' ]; then
+            RENNUM=$(expr $RENNUM + 1) 
+            $DBSTUB -dir $LDDEFDIR/directory -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},1,2,${16} > ${LOG_FILE}-21.log
+            if  [ -e ${16} ]; then
+                exit
+            fi
+        fi
+        
 
 	    $DBSTUB  -dir $LDDEFDIR/directory -bd orcabt ORCBJOB -parameter JBE${12}${13},${15}
 
