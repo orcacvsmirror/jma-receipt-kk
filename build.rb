e96ca571f3de0dbc73e5895c2817242115497236
tosihiko <tosihiko@cvs.orca.med.or.jp>
2012-10-27 10:21:00 +0900

    ビルド設定内容をconfigファイルとした

--- build.rb
+++ build.rb
@@ -1,14 +1,54 @@
 #!/usr/bin/ruby1.8
 
 require 'yaml'
+require 'logger'
 require 'fileutils'
+require 'optparse'
 require 'kconv'
 
-@baseurl = "http://ftp.orca.med.or.jp/pub/receipt/plugin/4.7.0/"
-@distdir = "dist"
-@list    = "plugin.yml"
-@keyuser = "plugintest"
-@passphrase = "test"
+def parse_option(argv0, argv)
+  spec = [
+    ["-c", "--config=VAL",      "config file",    :config],
+    ["-U", "--url=VAL",         "base url",       :baseurl],
+    ["-D", "--dist=VAL",        "dist dir",       :distdir],
+    ["-l", "--list=VAL",        "list",           :list],
+    ["-u", "--user=VAL",        "key user",       :keyuser],
+    ["-p", "--pass=VAL",        "pass phrase",    :passphrase],
+    ["-L", "--log=VAL",         "log",            :log],
+  ]
+  option = {}
+  parser = OptionParser.new{|opts|
+    opts.banner = "Usage: #{File.basename(argv0)} [option]\n"
+    opts.on_head("options:")
+    spec.each{|short, long, desc, key|
+      opts.on(short, long, desc){|arg|
+        option[key] = arg
+      }
+    }
+    opts.on("-d", "--debug", "debug mode") {
+      option[:debug] = true
+    }
+    opts.on_tail('--help', 'show this message'){
+      raise "help specified"
+    }
+  }
+  begin
+    parser.parse!(argv)
+  rescue RuntimeError => ex
+    puts parser
+    exit 1
+  end
+  config = YAML.load(File.read(option[:config])) rescue {}
+  log = Logger.new(option[:log] || STDERR)
+  log.level = option[:debug] ? Logger::DEBUG : Logger::INFO
+  config[:log] = log
+  config[:baseurl] = option[:baseurl] if option[:baseurl]
+  config[:distdir] = option[:distdir] if option[:distdir]
+  config[:list] = option[:list] if option[:list]
+  config[:keyuser] = option[:keyuser] if option[:keyuser]
+  config[:passphrase] = option[:passphrase] if option[:passphrase]
+  return config
+end
 
 def check_control(c)
   raise "invalid control: no name" unless c[:name] 
@@ -18,8 +58,8 @@ end
 
 def clean
   # dist clean
-  FileUtils.rm_rf(@distdir)
-  FileUtils.mkdir_p(@distdir)
+  FileUtils.rm_rf(@opts[:distdir])
+  FileUtils.mkdir_p(@opts[:distdir])
 end
 
 def make_list
@@ -44,7 +84,7 @@ def make_list
   
   str = "---\n"
   list.each do |c|
-    url = @baseurl + c[:filename]
+    url = @opts[:baseurl] + c[:filename]
     str << %|  - :name: "#{c[:name]}"\n|
     str << %|    :version: "#{c[:version]}"\n|
     str << %|    :vendor: "#{c[:vendor]}"\n|
@@ -52,7 +92,7 @@ def make_list
     str << %|    :date: #{date}\n|
     str << %|    :url: #{url}\n|
   end
-  listfile = File.join(@distdir,@list)
+  listfile = File.join(@opts[:distdir],@opts[:list])
   File.open(listfile,"w") do|io| io.puts str end
   sign(listfile)
   list
@@ -69,22 +109,26 @@ def exec(command)
 end
 
 def sign(file)
-  if @passphrase.nil? || @passphrase.empty?
-    exec("gpg -u #{@keyuser} -abs #{file}")
+  if @opts[:passphrase].nil? || @opts[:passphrase].empty?
+    exec("gpg -u #{@opts[:keyuser]} -abs #{file}")
   else
-    exec("gpg --passphrase #{@passphrase} -u #{@keyuser} -abs #{file}")
+    exec("gpg --passphrase #{@opts[:passphrase]} -u #{@opts[:keyuser]} -abs #{file}")
   end
 end
 
 def make_package(list)
   list.each do |c|
-    pkgfile = File.join(@distdir,c[:filename])
-    exec("tar zcvf #{pkgfile} #{c[:dir]}")
+    directory = c[:name] + "-" + c[:version]
+    pkgfile = File.join(@opts[:distdir],c[:filename])
+    exec("cp -rp #{c[:dir]} #{directory}")
+    exec("tar zcvf #{pkgfile} #{directory}")
+    exec("rm -r #{directory}")
     sign(pkgfile)
   end
 end
 
 if $0 == __FILE__
+  @opts = parse_option($0, ARGV)
   clean
   make_package(make_list)
 end
