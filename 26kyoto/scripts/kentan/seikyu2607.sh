e14eddcf93229e0539e0d05004ab81e090e842b8
yoshi <yoshi@cvs.orca.med.or.jp>
2012-11-14 12:59:00 +0900

    ${LDDEFDIR}/directory -> ${LDDIRECTORY}

--- 26kyoto/scripts/kentan/seikyu2607.sh
+++ 26kyoto/scripts/kentan/seikyu2607.sh
@@ -26,11 +26,11 @@ RENNUM=0
         cd  ${ORCA_DIR}
 
             RENNUM=$(expr ${RENNUM} + 1) 
-            $DBSTUB -dir $LDDEFDIR/directory -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${14},${16} > ${LOG_FILE}.log 2>&1
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${14},${16} > ${LOG_FILE}.log 2>&1
             if  [ -e ${15} ]; then
                 exit
             fi
 
-        $DBSTUB  -dir $LDDEFDIR/directory -bd orcabt ORCBJOB -parameter JBE${12}${13},${14}
+        $DBSTUB  -dir $LDDIRECTORY -bd orcabt ORCBJOB -parameter JBE${12}${13},${14}
 
         exit
