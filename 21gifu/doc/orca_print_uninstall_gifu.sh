e83591e377423f2c61f0c7229368b9d57a5f1767
yoshi <yoshi@cvs.orca.med.or.jp>
2013-03-22 10:00:54 +0900

    アンインストールで削除されないファイルを削除

--- 21gifu/doc/orca_print_uninstall_gifu.sh
+++ 21gifu/doc/orca_print_uninstall_gifu.sh
@@ -88,6 +88,14 @@ for f in ${SITESRCDIR}/scripts/kentan/sokatu${prefno}* ; do
   rm -rf $f
 done
 
+if test -f ${SITESRCDIR}/data/YUSEN.INI ; then
+  rm -rf ${SITESRCDIR}/data/YUSEN.INI
+fi
+
+if test -f ${SITESRCDIR}/doc/orca_print_uninstall_${prefname}.sh ; then
+  rm -rf ${SITESRCDIR}/doc/orca_print_uninstall_${prefname}.sh
+fi
+
 
 for f in ${SITELIBDIR}/SEIKYU${prefno}* ; do
   rm -rf $f
@@ -129,6 +137,16 @@ done
 if test -f ${LDDEFDIR}/${prefname}.bd ; then
   rm -rf ${LDDEFDIR}/${prefname}.bd
 fi
+
+
+if test -f ${SITEDATADIR}/YUSEN.INI ; then
+  rm -rf ${SITEDATADIR}/YUSEN.INI 
+fi
+
+if test -f ${SITELIBDIR}/doc/orca_print_uninstall_${prefname}.sh ; then
+  rm -rf ${SITELIBDIR}/doc/orca_print_uninstall_${prefname}.sh
+fi
+
 
 echomsg "岐阜県(総括表・地方公費)アンインストール終了！！"
 
