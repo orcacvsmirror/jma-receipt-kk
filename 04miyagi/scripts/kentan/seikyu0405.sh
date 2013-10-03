0a0dc83b24fb59247f4c3655be9e3765fbf274fa
yoshi <yoshi@cvs.orca.med.or.jp>
2013-10-03 13:43:12 +0900

    INIファイル作成処理を削除

--- 04miyagi/scripts/kentan/seikyu0405.sh
+++ 04miyagi/scripts/kentan/seikyu0405.sh
@@ -5,8 +5,6 @@ PREFNAME=miyagi
 PROGRAMID=SEIKYU0405
 LOG_FILE="/var/log/jma-receipt/${14}seikyu0405"
 RENNUM=0
-PRGOPT="select option from tbl_prgoption where hospnum=${14} and prgid='${PROGRAMID}' and kbncd='TAISYOKOHI';"
-INIFILE="/tmp/${14}${PROGRAMID}TAISYOKOHI.INI"
 #-------------------------------------------#
 #    地方公費作成（宮城・乳幼児）
 #        $1-${11}
@@ -25,9 +23,6 @@ INIFILE="/tmp/${14}${PROGRAMID}TAISYOKOHI.INI"
             rm  ${15}
         fi
 
-##      INIファイル 作成
-        echo "${PRGOPT}" | psql -At ${DBNAME} > ${INIFILE}
-
         cd  ${ORCA_DIR}
 
             RENNUM=$(expr ${RENNUM} + 1) 
@@ -35,9 +30,6 @@ INIFILE="/tmp/${14}${PROGRAMID}TAISYOKOHI.INI"
             if  [ -e ${15} ]; then
                 exit
             fi
-
-##      INIファイル 削除
-        rm -f ${INIFILE}
 
         $DBSTUB  -dir $LDDIRECTORY -bd orcabt ORCBJOB -parameter JBE${12}${13},${14}
 
