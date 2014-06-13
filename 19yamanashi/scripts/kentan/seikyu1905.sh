6b9dc6541fd5483d2719ccb2bdf7af9401123352
yoshi <yoshi@cvs.orca.med.or.jp>
2014-06-13 14:43:32 +0900

    「複写式電子レセプト」新規作成

---
+++ 19yamanashi/scripts/kentan/seikyu1905.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+. @jma-receipt-env@
+
+PREFNAME=yamanashi
+PROGRAMID=SEIKYU1905
+LOG_FILE="/var/log/jma-receipt/${14}seikyu1905"
+RENNUM=0
+PRGOPT="select option from tbl_prgoption where hospnum=${14} and prgid='${PROGRAMID}' and kbncd='RECEDENPATH';"
+RECEDENTMP=/var/tmp/${14}RECEDEN-TMP.CSV
+#-------------------------------------------#
+#    地方公費作成 複写式電子レセプト
+#        $1-${11}
+#              印刷ＤＢ用定固定引数(CPORCSRTLNK.INC)
+#        ${12} ジョブＩＤ
+#        ${13} シェルＩＤ
+#        ${14} 医療機関ＩＤ 
+#        ${15} エラーファイル名 
+#        ${16} 処理区分(０：一括、１：個別)
+#-------------------------------------------#
+#
+##      エラーファイル削除
+	echo $#
+	echo "echo " ${15}
+        if  [ -e ${15} ]; then
+            rm  ${15}
+        fi
+
+        cd  ${ORCA_DIR}
+
+##          PGオプションからパスを取得し、TMPへコピー
+            RECEDENPATH=`echo "${PRGOPT}" | psql -At ${DBNAME}`
+            cp ${RECEDENPATH} ${RECEDENTMP}
+            
+##          文字コードをEUCに変換
+            nkf -Se --overwrite ${RECEDENTMP}
+
+            RENNUM=$(expr ${RENNUM} + 1) 
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${14},${16},${15} > ${LOG_FILE}.log 2>&1
+            if  [ -e ${15} ]; then
+                exit
+            fi
+
+##          TMPファイル削除
+            rm -rf ${RECEDENTMP}
+
+        $DBSTUB  -dir $LDDIRECTORY -bd orcabt ORCBJOB -parameter JBE${12}${13},${14}
+
+        exit
