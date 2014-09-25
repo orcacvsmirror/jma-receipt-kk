e01221398a166a740ac213f98f18a2d41864c016
yoshi <yoshi@cvs.orca.med.or.jp>
2014-09-25 14:00:53 +0900

    元に戻す

--- build.rb
+++ build.rb
@@ -94,7 +94,6 @@ def make_list
   end
   listfile = File.join(@opts[:distdir],@opts[:list])
   File.open(listfile,"w") do|io| io.puts str end
-  exec("nkf -w --overwrite listfile")
   sign(listfile)
   list
 end
