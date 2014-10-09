5092622f0faf66f1a8cff1ec347844a0514aeb75
yoshi <yoshi@cvs.orca.med.or.jp>
2014-10-09 18:33:47 +0900

    シス管「3004」自動登録のCOBOL対応

--- 01hokkaido/scripts/syskanri/sys3004.sh
+++ 01hokkaido/scripts/syskanri/sys3004.sh
@@ -12,13 +12,52 @@ fi
 HOSPNUM="01"
 POST=`pwd`
 
-# syskanri 3004 set
+PREFNAME=sys3004
+PROGRAMID=SYS3004
+LOG_FILE="/var/log/jma-receipt/hokkaido-plugin"
+
 cd ${SITESRCDIR}/scripts/syskanri
-export APS_EXEC_PATH=`pwd`
+###export APS_EXEC_PATH=`pwd`
+#cd  ${ORCA_DIR}
+
+# compile COBOL programs
+MODULES=${PROGRAMID}.CBL
+for f in $MODULES; do
+  if test "x`echo -n $f | grep 'CBL$'`" != "x"; then
+    m=`echo $f | sed 's/CBL$/so/'`
+    echo -n "Building ${m}..."
+    ${COBOL} ${COBOLFLAGS} -o ${SITELIBDIR}/${m} \
+         -I ${PATCHCOPYDIR} \
+         -I ${COPYDIR} \
+         -I ${SITESRCDIR}/cobol/copy \
+        ${f}
+    echo "done"
+  fi
+done
+
+#------------------------------------------------------
+#     シス管「3004」登録
+#     NOWYMD    現在日付
+#     NOWHMS    現在時間
+#     NOWDIR    現在ディレクトリ
+#     FILENAME  ファイル名
+#------------------------------------------------------
+NOWYMD=$(date +"%Y%m%d")
+NOWHMS=$(date +"%H%M%S")
+NOWDIR=$(pwd)
+FILENAME=sys3004.data
+echo  "NOWDIR  :  "  ${NOWDIR}
+
+#test
+#COB_LIBRARY_PATH="$SITELIBDIR":"$PATCHLIBDIR":"$ORCALIBDIR":"$PANDALIB"
+#export COB_LIBRARY_PATH
 
 ln -s $SYSCONFDIR/dbgroup.inc dbgroup.inc
 
-$DBSTUB -bd sys3004 Sys3004 -parameter $HOSPNUM
+# syskanri 3004 set
+$DBSTUB -dir ${NOWDIR}/directory -bd $PREFNAME -ddir ${NOWDIR} $PROGRAMID -parameter ${HOSPNUM},${NOWYMD},${NOWHMS},${NOWDIR},${FILENAME} > ${LOG_FILE}.log 2>&1
+#so del
+rm ${SITEDIR}/${PROGRAMID}.so
 
 cd $POST
 
