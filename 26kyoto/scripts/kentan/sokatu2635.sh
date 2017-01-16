625f39949bcf2a42361e2db8c4fab535005d5b56
yoshi <yoshi@cvs.orca.med.or.jp>
2017-01-16 10:56:04 +0900

    特別療養費国保分は別帳票とする

--- 26kyoto/scripts/kentan/sokatu2635.sh
+++ 26kyoto/scripts/kentan/sokatu2635.sh
@@ -44,6 +44,22 @@ RENNUM=0
                 exit
             fi
         fi
+##      返戻分（特別療養費分）
+        if  [ ${19} -eq '0' ] || [ ${19} -eq '2' ]; then
+            RENNUM=$(expr $RENNUM + 1) 
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${15},${12},${13},${18},2,2,${16} > ${LOG_FILE}-22.log 2>&1
+            if  [ -e ${MCP_TEMPDIR}/${16} ]; then
+                exit
+            fi
+        fi
+##      当月・月遅れ分（特別療養費分）
+        if  [ ${19} -eq '0' ] || [ ${19} -eq '1' ]; then
+            RENNUM=$(expr $RENNUM + 1) 
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${15},${12},${13},${18},1,2,${16} > ${LOG_FILE}-21.log 2>&1
+            if  [ -e ${MCP_TEMPDIR}/${16} ]; then
+                exit
+            fi
+        fi
 
 	$DBSTUB  -dir $LDDIRECTORY -bd orcabt ORCBJOB -parameter JBE${12}${13},${15}
 
