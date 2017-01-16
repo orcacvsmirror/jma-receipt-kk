a431d242f6b92d246cd5a5d332bd42fb5b10c8e3
yoshi <yoshi@cvs.orca.med.or.jp>
2017-01-16 12:54:47 +0900

    特別療養費国保分は別帳票とする 医療機関コード変更時のレセ電データ等の作成対応（１日からの変更のみ）

--- 26kyoto/scripts/kentan/sokatu2612.sh
+++ 26kyoto/scripts/kentan/sokatu2612.sh
@@ -15,6 +15,8 @@ RENNUM=0
 #        ${16} エラーファイル名 
 #        ${19} 処理対象区分
 #              0:全体  1:当月・月遅れ分  2:返戻分
+#        ${20}-  ${21}医療機関コード別の処理指定時
+#            該当診療年月の開始、終了
 #-------------------------------------------#
 #
 ##      エラーファイル削除
@@ -27,7 +29,7 @@ RENNUM=0
 ##      返戻分
         if  [ ${19} -eq '0' ] || [ ${19} -eq '2' ]; then
             RENNUM=$(expr $RENNUM + 1) 
-            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${15},${12},${13},2,${16} > ${LOG_FILE}-2.log 2>&1
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${15},${12},${13},2,1,${20},${21},${16} > ${LOG_FILE}-2.log 2>&1
             if  [ -e ${MCP_TEMPDIR}/${16} ]; then
                 exit
             fi
@@ -35,11 +37,27 @@ RENNUM=0
 ##      当月・月遅れ分
         if  [ ${19} -eq '0' ] || [ ${19} -eq '1' ]; then
             RENNUM=$(expr $RENNUM + 1) 
-            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${15},${12},${13},1,${16} > ${LOG_FILE}-1.log 2>&1
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${15},${12},${13},1,1,${20},${21},${16} > ${LOG_FILE}-1.log 2>&1
             if  [ -e ${MCP_TEMPDIR}/${16} ]; then
                 exit
             fi
         fi
-        
+##      返戻分（特別療養費分）
+        if  [ ${19} -eq '0' ] || [ ${19} -eq '2' ]; then
+            RENNUM=$(expr $RENNUM + 1) 
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${15},${12},${13},2,2,${20},${21},${16} > ${LOG_FILE}-22.log 2>&1
+            if  [ -e ${MCP_TEMPDIR}/${16} ]; then
+                exit
+            fi
+        fi
+##      当月・月遅れ分（特別療養費分）
+        if  [ ${19} -eq '0' ] || [ ${19} -eq '1' ]; then
+            RENNUM=$(expr $RENNUM + 1) 
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${15},${12},${13},1,2,${20},${21},${16} > ${LOG_FILE}-21.log 2>&1
+            if  [ -e ${MCP_TEMPDIR}/${16} ]; then
+                exit
+            fi
+        fi
+
 	$DBSTUB  -dir $LDDIRECTORY -bd orcabt ORCBJOB -parameter JBE${12}${13},${15}
 
