f727f33a30607f43f5e16e175235c990119a1d2e
monma <monma@cvs.orca.med.or.jp>
2017-07-10 16:01:26 +0900

    医療機関コード変更時、医療機関コード別の作成処理対応

--- 37kagawa/scripts/kentan/sokatu3700.sh
+++ 37kagawa/scripts/kentan/sokatu3700.sh
@@ -13,12 +13,14 @@ RENNUM=0
 #        ${13} シェルＩＤ
 #        ${14} 総括表の種類
 #           0:全体  1:社保　2:国保
-#        ${15} 医療機関ＩＤ 
+#        ${15} 医療機関番号
 #        ${16} エラーファイル名 
 #        ${17} 処理年月 
 #        ${18} 対象保険者番号
 #        ${19} 処理対象区分
 #              0:全体  1:当月・月遅れ分  2:返戻分
+#        ${20} - ${21} 医療機関コード別の処理指定時
+#                      該当診療年月の開始、終了
 #-------------------------------------------#
 #
 ##      エラーファイル削除
@@ -33,7 +35,7 @@ RENNUM=0
 ##      返戻分
         if  [ ${19} -eq '0' ] || [ ${19} -eq '2' ]; then
             RENNUM=$(expr ${RENNUM} + 1) 
-            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},2,1,${16} > ${LOG_FILE}-2.log 2>&1
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},2,1,${20},${21},${16} > ${LOG_FILE}-2.log 2>&1
             if  [ -e ${MCP_TEMPDIR}/${16} ]; then
                 exit
             fi
@@ -41,7 +43,7 @@ RENNUM=0
 ##      当月・月遅れ分
         if  [ ${19} -eq '0' ] || [ ${19} -eq '1' ]; then
             RENNUM=$(expr ${RENNUM} + 1) 
-            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},1,1,${16} > ${LOG_FILE}-1.log 2>&1
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},1,1,${20},${21},${16} > ${LOG_FILE}-1.log 2>&1
             if  [ -e ${MCP_TEMPDIR}/${16} ]; then
                 exit
             fi
@@ -49,7 +51,7 @@ RENNUM=0
 ##      返戻分（特別療養費分）
         if  [ ${19} -eq '0' ] || [ ${19} -eq '2' ]; then
             RENNUM=$(expr ${RENNUM} + 1) 
-            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},2,2,${16} > ${LOG_FILE}-22.log 2>&1
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},2,2,${20},${21},${16} > ${LOG_FILE}-22.log 2>&1
             if  [ -e ${MCP_TEMPDIR}/${16} ]; then
                 exit
             fi
@@ -57,7 +59,7 @@ RENNUM=0
 ##      当月・月遅れ分（特別療養費分）
         if  [ ${19} -eq '0' ] || [ ${19} -eq '1' ]; then
             RENNUM=$(expr ${RENNUM} + 1) 
-            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},1,2,${16} > ${LOG_FILE}-21.log 2>&1
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${15},1,2,${20},${21},${16} > ${LOG_FILE}-21.log 2>&1
             if  [ -e ${MCP_TEMPDIR}/${16} ]; then
                 exit
             fi
