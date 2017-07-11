5c91b4bcf72f2c1a000542229c4e87a51323fd54
monma <monma@cvs.orca.med.or.jp>
2017-07-11 19:07:55 +0900

    バッチログ画面対応

--- 19yamanashi/scripts/kentan/seikyu1905.sh
+++ 19yamanashi/scripts/kentan/seikyu1905.sh
@@ -17,9 +17,11 @@ KORECEDENTMP=${14}KORECEDEN-TMP.CSV
 #              印刷ＤＢ用定固定引数(CPORCSRTLNK.INC)
 #        ${12} ジョブＩＤ
 #        ${13} シェルＩＤ
-#        ${14} 医療機関ＩＤ 
+#        ${14} 医療機関番号
 #        ${15} エラーファイル名 
 #        ${16} 処理区分(０：一括、１：個別)
+#        ${17} - ${18}医療機関コード別の処理指定時
+#              該当診療年月の開始、終了
 #-------------------------------------------#
 #
 ##      エラーファイル削除
@@ -47,7 +49,7 @@ KORECEDENTMP=${14}KORECEDEN-TMP.CSV
             fi
 
             RENNUM=$(expr ${RENNUM} + 1) 
-            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${14},${16},${15},${RECEDENTMP},${SYARECEDENTMP},${KORECEDENTMP} > ${LOG_FILE}.log 2>&1
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${14},${16},${15},${RECEDENTMP},${SYARECEDENTMP},${KORECEDENTMP}
             if  [ -e ${MCP_TEMPDIR}/${15} ]; then
                 exit
             fi
