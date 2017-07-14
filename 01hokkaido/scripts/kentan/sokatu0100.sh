84c1bb20d30b183be11ba6072c10f54419f4c057
yoshi <yoshi@cvs.orca.med.or.jp>
2017-07-14 16:18:53 +0900

    修正

--- 01hokkaido/scripts/kentan/sokatu0100.sh
+++ 01hokkaido/scripts/kentan/sokatu0100.sh
@@ -5,7 +5,7 @@ PREFNAME=hokkaido
 PROGRAMID=SOKATU0100
 LOG_FILE="/var/log/jma-receipt/${15}sokatu0100"
 RENNUM=0
--------------------------------------------#
+#-------------------------------------------#
 #    国保総括表作成（北海道）
 #        $1-${11}
 #              印刷ＤＢ用定固定引数(CPORCSRTLNK.INC)
