abbb8e6e2a20e19b4396f58636e3b863b7f7e982
yoshi <yoshi@cvs.orca.med.or.jp>
2017-08-30 17:17:23 +0900

    ginbee対応

--- 19yamanashi/scripts/kentan/seikyu1905.sh
+++ 19yamanashi/scripts/kentan/seikyu1905.sh
@@ -6,13 +6,8 @@ PREFNAME=yamanashi
 PROGRAMID=SEIKYU1905
 LOG_FILE="/var/log/jma-receipt/${14}seikyu1905"
 RENNUM=0
-PRGOPT="select option from tbl_prgoption where hospnum=${14} and prgid='${PROGRAMID}' and kbncd='RECEDENPATH';"
-PRGOPT2="select option from tbl_prgoption where hospnum=${14} and prgid='${PROGRAMID}' and kbncd='SITEIKBN';"
-RECEDENTMP=${14}RECEDEN-TMP.CSV
-SYARECEDENTMP=${14}SYARECEDEN-TMP.CSV
-KORECEDENTMP=${14}KORECEDEN-TMP.CSV
 #-------------------------------------------#
-#    地方公費作成 複写式電子レセプト
+#    地方公費作成 複写式電子レセプト（クラウド対応）
 #        $1-${11}
 #              印刷ＤＢ用定固定引数(CPORCSRTLNK.INC)
 #        ${12} ジョブＩＤ
@@ -33,31 +28,11 @@ KORECEDENTMP=${14}KORECEDEN-TMP.CSV
 
         cd  ${ORCA_DIR}
 
-##          PGオプションからパスを取得し、TMPへコピー
-            RECEDENPATH=`echo "${PRGOPT}" | psql -At ${DBNAME}`
-            SITEIKBN=`echo "${PRGOPT2}" | psql -At ${DBNAME}`
-            if [ $SITEIKBN = 1 ]; then
-                cp ${RECEDENPATH} ${MCP_TEMPDIR}/${RECEDENTMP}
-#               文字コードをEUCに変換
-                nkf -Se --overwrite ${MCP_TEMPDIR}/${RECEDENTMP}
-            else
-                cp ${RECEDENPATH}/${14}_Shaho_${5}/RECEIPTC.UKE  ${MCP_TEMPDIR}/${SYARECEDENTMP}
-                cp ${RECEDENPATH}/${14}_Kokuho_${5}/RECEIPTC.UKE ${MCP_TEMPDIR}/${KORECEDENTMP}
-#               文字コードをEUCに変換
-                nkf -Se --overwrite ${MCP_TEMPDIR}/${SYARECEDENTMP}
-                nkf -Se --overwrite ${MCP_TEMPDIR}/${KORECEDENTMP}
-            fi
-
             RENNUM=$(expr ${RENNUM} + 1) 
-            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${14},${16},${15},${RECEDENTMP},${SYARECEDENTMP},${KORECEDENTMP}
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${14},${16},${17},${18},${15}
             if  [ -e ${MCP_TEMPDIR}/${15} ]; then
                 exit
             fi
-
-##          TMPファイル削除
-            rm -rf ${MCP_TEMPDIR}/${RECEDENTMP}
-            rm -rf ${MCP_TEMPDIR}/${SYARECEDENTMP}
-            rm -rf ${MCP_TEMPDIR}/${KORECEDENTMP}
 
         $DBSTUB  -dir $LDDIRECTORY -bd orcabt ORCBJOB -parameter JBE${12}${13},${14}
 
