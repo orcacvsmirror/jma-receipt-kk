036024d80814e469f8b4567f2d9718050644c2ec
yoshi <yoshi@cvs.orca.med.or.jp>
2017-09-14 19:20:00 +0900

    バッチログ画面対応

--- 47okinawa/scripts/kentan/seikyu4707.sh
+++ 47okinawa/scripts/kentan/seikyu4707.sh
@@ -28,7 +28,7 @@ RENNUM=0
         cd  ${ORCA_DIR}
 
             RENNUM=$(expr ${RENNUM} + 1) 
-            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${14},${16},${15} > ${LOG_FILE}.log 2>&1
+            $DBSTUB -dir $LDDIRECTORY -bd $PREFNAME $PROGRAMID -parameter $1,$2,$3,$RENNUM,$5,$6,$7,$8,$9,${10},${11},${12},${13},${14},${16},${15}
             if  [ -e ${MCP_TEMPDIR}/${15} ]; then
                 exit
             fi
