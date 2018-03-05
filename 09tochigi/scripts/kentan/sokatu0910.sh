6ae8d29c98a875a564da52c80c8293264bd2372c
monma <monma@cvs.orca.med.or.jp>
2018-03-05 10:32:26 +0900

    記述漏れ等を修正

--- 09tochigi/scripts/kentan/sokatu0910.sh
+++ 09tochigi/scripts/kentan/sokatu0910.sh
@@ -5,7 +5,7 @@ PREFNAME=tochigi
 PROGRAMID=SOKATU0910
 LOG_FILE="/var/log/jma-receipt/${15}sokatu0910"
 RENNUM=0
--------------------------------------------#
+#-------------------------------------------#
 #    国保・診療報酬請求書（栃木）
 #        $1-${11}
 #              印刷ＤＢ用定固定引数(CPORCSRTLNK.INC)
